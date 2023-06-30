d7bd985dacea0a766ef989a3586f193a110aab14
Kauff <matthew.kauff@maxar.com>
2023-06-30 18:07:31 -0400

    add 1.1 and 1.2 react intro

--- part1/part1/src/App.js
+++ part1/part1/src/App.js
@@ -1,43 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-const Hello = () => {
-  return (
-    <div>
-      <p>Hello world</p>
-    </div>
-  )
-}
-
 const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  const Content = () => {
+    return (
+      <div>
+        <Part name={part1 + " " + exercises1}/>
+        <Part name={part2 + " " + exercises2}/>
+        <Part name={part3 + " " + exercises3}/>
+      </div>
+    )
+  }
+
   return (
     <div>
-      <h1>Greetings</h1>
-
-      <Hello />
+      <Header course={course}/>
+      <Content/>
+      {/* <Content name={part2 + " " + exercises2}/>
+      <Content name={part3 + " " + exercises3}/> */}
+      <Total sum={exercises1 + exercises2 + exercises3}/>
     </div>
   )
 }
 
-export default App;
+export default App
+
+function Header(props) {
+  return (
+    <h1>{props.course}</h1>
+  );
+}
+
+
+
+function Part(props) {
+  return (
+    <p>{props.name}</p>
+  );
+}
+
+function Total(props) {
+  return (
+    <p>TOTAL {props.sum}</p>
+  );
+}
